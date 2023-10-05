1f630bd05b69d6fe4cc22fb37709c4467fc2137d
Devesh Yadav <deveshamway@gmail.com>
2023-10-05 13:30:00 +0530

    Time: 7 ms (87.90%), Space: 16.2 MB (28.20%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div229-majority-element-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div229-majority-element-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Extended moore voting algo
+        
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+        
+        for(int i = 0; i<n; i++){
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1 = 1;
+                el1 = nums[i];
+            }
+            else if(cnt2==0 && el1!=nums[i]){
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i]==el1){cnt1++;}
+            else if(nums[i]==el2){cnt2++;}
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        vector<int>ans;
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i]==el1){cnt1++;}
+            if(nums[i]==el2){cnt2++;}
+        }
+        
+        int mini = (n/3)+1;
+        if(cnt1>=mini){
+            ans.push_back(el1);
+        }
+        if(cnt2>=mini && el1!=el2){
+            ans.push_back(el2);
+        }
+        
+        return ans;
+    }
+};
