50610caee7029ff6cc882e34a8124a08d6f1dd14
Devesh Yadav <deveshamway@gmail.com>
2024-06-29 12:45:12 +0530

    Time: 3 ms (71.28%), Space: 12 MB (85.08%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& arr, int x){
-        int n = arr.size();
-        int idx = n;
-        int l = 0, r = n-1;
-        while(l<=r){
-            int m = l + (r-l)/2;
-            if(arr[m]>=x){
-                idx = min(idx,m);
-                r = m-1;
-            }
-            else{
-                l = m+1;
-            }
-        }
-        return idx;
+    int searchInsert(vector<int>& nums, int target) {
+        // in short we need to find smallest index st arr[i]>=target : lower bound
+        return lower_bound(nums.begin(),nums.end(),target) - nums.begin();
     }
 };
